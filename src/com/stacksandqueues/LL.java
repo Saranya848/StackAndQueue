503a8e02b1891cc22554ba03850b11eb9aec2139
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-04 02:26:34 +0530

    Ability to peek and pop of stack till empty

--- src/com/stacksandqueues/LL.java
+++ src/com/stacksandqueues/LL.java
@@ -1,10 +1,16 @@
 package com.stacksandqueues;
 
+import java.util.EmptyStackException;
+
 public class LL {
 	/**
-	 * Method for push Method for print
+	 * pop - this method is used to remote element from top of the stack isEmpty -
+	 * this method is used to check stack is empty or not peek - this method returns
+	 * peek element of the stack Method for push Method for print
 	 */
+	Node top;
 	Node head;
+	int size;
 
 	/**
 	 * Method for push
@@ -15,6 +21,36 @@ public class LL {
 		Node newnode = new Node(data);
 		newnode.next = head;
 		head = newnode;
+	}
+
+	public int pop() {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		int result = top.data;
+		top = top.next;
+		size--;
+		return result;
+	}
+
+	/**
+	 * isEmpty - this method is used to check stack is empty or not
+	 * 
+	 */
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	/**
+	 * peek - this method returns peek element of the stack
+	 * 
+	 */
+	public int peek() {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		return top.data;
+
 	}
 
 	/**
--- src/com/stacksandqueues/Stack.java
+++ src/com/stacksandqueues/Stack.java
@@ -5,6 +5,10 @@ public class Stack {
 	* PROCEDURE
 	* 1. Creating Linked list
 	* 2. pushing values to stack
+	* 3. Displaying the stack
+	* 4. check stack empty or not
+	* 5.display peek element of the stack
+	* 6.pop element of the stack
 	*/
 	
 	/*
@@ -23,4 +27,17 @@ public class Stack {
 	 * 3.Displaying the stack
 	 */
 	list.printLL();
+	
+	/*
+	 * 4.check stack empty or not 
+	 */
+	System.out.println("Stack is empty or not  "+list.isEmpty());
+	/*
+	 * 5.display peek element of the stack 
+	 */
+	System.out.println("Peek element of stack  "+list.peek());
+	/*
+	 * 6.pop element of the stack
+	 */
+	System.out.println("pop element of stack  "+list.pop());
 }
